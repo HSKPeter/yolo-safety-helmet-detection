a0560eb1d00580c01d4c949bdbb4a27ee88e61e6
Peter Hui <huishikuen@gmail.com>
2023-11-14 15:05:38 -0800

    Add coco8.yaml to train yolov8

--- src/model/main.py
+++ src/model/main.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
 import os
 
-from util.config import IMAGE_FOLDER_PATH
+from util.config import EPOCHS_COUNT, IMAGE_FOLDER_PATH, YML_DATASET_PATH
 
 def main():
     print("Loading pretrained YOLOv8 model...")
@@ -9,6 +9,16 @@ def main():
     path_of_pretrained_model = os.path.join(current_dir, "yolov8n.pt")
     model = YOLO(path_of_pretrained_model)
     print("Loaded pretrained YOLOv8 model.")
+
+    # Train the model using the 'coco8.yaml' dataset for 3 epochs
+    yaml_file_name = "coco8.yaml"
+    print(f"Training model with {yaml_file_name} dataset...")
+    yaml_file_path = os.path.join(YML_DATASET_PATH, yaml_file_name)
+    results = model.train(data=yaml_file_path, epochs=EPOCHS_COUNT)
+
+    # Evaluate the model's performance on the validation set
+    print("Evaluating model...")
+    results = model.val()
 
     image_path = os.path.join(IMAGE_FOLDER_PATH, "bus.jpg")
     print(f"Predicting image at {image_path}...")
--- src/util/config.py
+++ src/util/config.py
@@ -1,6 +1,11 @@
 import os
 
+EPOCHS_COUNT = 3
 
 RANDOM_STATE = 42
 
-IMAGE_FOLDER_PATH = os.path.join(os.path.dirname(__file__), os.pardir, "assets", "img")
+IMAGE_FOLDER_PATH = os.path.join(
+    os.path.dirname(__file__), os.pardir, "assets", "img")
+
+YML_DATASET_PATH = os.path.join(os.path.dirname(
+    __file__), os.pardir, "assets", "datasets")
