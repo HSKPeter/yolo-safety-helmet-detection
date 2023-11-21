ea55b78813f4ac06d8817d3d81e36fcd7b535788
Peter Hui <huishikuen@gmail.com>
2023-11-21 15:34:48 -0800

    Add more custom models

--- src/util/config.py
+++ src/util/config.py
@@ -8,6 +8,9 @@ ASSETS_FOLDER_PATH = os.path.join(
     os.path.dirname(__file__), os.pardir, "assets")
 
 IMAGE_FOLDER_PATH = os.path.join(ASSETS_FOLDER_PATH, "img")
+VIDEO_FOLDER_PATH = os.path.join(ASSETS_FOLDER_PATH, "video")
 YML_DATASET_PATH = os.path.join(ASSETS_FOLDER_PATH, "yolo_yaml")
 
 PRETRAINED_MODEL_PATH = os.path.join(ASSETS_FOLDER_PATH, "pretrained_models")
+
+CUSTOM_MODEL_PATH = os.path.join(ASSETS_FOLDER_PATH, "models")
--- src/yolov5_custom.py
+++ src/yolov5_custom.py
@@ -1,16 +1,23 @@
 import torch
 import os
 
-from util.config import IMAGE_FOLDER_PATH, PRETRAINED_MODEL_PATH
+from util.config import IMAGE_FOLDER_PATH, CUSTOM_MODEL_PATH
 
 def main():
-    pretrained_yolo_v5_model_path = os.path.join(PRETRAINED_MODEL_PATH, "yolov5s_custom_1.pt")
-    model = torch.hub.load('ultralytics/yolov5', 'custom', path=pretrained_yolo_v5_model_path)
-    img_path = os.path.join(IMAGE_FOLDER_PATH, "helmet.jpg")
+    custom_yolo_v5_model_path = os.path.join(CUSTOM_MODEL_PATH, "custom_0.pt")
 
+    model = torch.hub.load('ultralytics/yolov5', 'custom', path=custom_yolo_v5_model_path)
+    model.conf = 0.2
+    LABEL_FOR_HARDHAT = 0
+    LABEL_FOR_NO_HARDHAT = 2
+    LABEL_FOR_PERSON = 5
+    model.classes = [LABEL_FOR_HARDHAT, LABEL_FOR_NO_HARDHAT, LABEL_FOR_PERSON]
+
+    img_path = os.path.join(IMAGE_FOLDER_PATH, "no_helmet", "no_helmet_1.jpg")
     results = model(img_path)
+    
     results.show()
-    results.save()   
+    results.save()
 
 
 if __name__ == "__main__":
