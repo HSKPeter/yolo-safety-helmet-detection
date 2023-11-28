defc0505fa469a8751d749105040f9308e19bcf3
Peter Hui <huishikuen@gmail.com>
2023-11-28 14:57:06 -0800

    Add script for data exploration

---
+++ src/exploration/count_label.py
@@ -0,0 +1,60 @@
+import os
+import matplotlib.pyplot as plt
+
+# Define the path to the labels folder
+folder_of_this_file = os.path.dirname(__file__)
+labels_folder_path = os.path.join(folder_of_this_file, 'labels')
+
+# Read all filenames in the labels folder
+filenames = os.listdir(labels_folder_path)
+
+# Filter txt files
+filenames = [x for x in filenames if x.endswith('.txt')]
+
+labels = {}
+
+
+def get_lines_from_file(filepath):
+    f = open(filepath, 'r')
+    content = f.read()
+    lines = content.splitlines()
+    f.close()
+    return lines
+
+
+def get_labels_from_lines(lines):
+    labels = []
+    for line in lines:
+        try:
+            line_split = line.split(' ')
+            label = line_split[0]
+            labels.append(label)
+        except:
+            pass
+    return labels
+
+
+for filename in filenames:
+    filepath = os.path.join(labels_folder_path, filename)
+    lines = get_lines_from_file(filepath)
+    labels_from_lines = get_labels_from_lines(lines)
+
+    for label in labels_from_lines:
+        if label in labels:
+            labels[label] += 1
+        else:
+            labels[label] = 1
+
+# Draw a pie chart with the labels
+labels_to_plot = []
+sizes = []
+for label in labels:
+    labels_to_plot.append(label)
+    sizes.append(labels[label])
+
+print(labels)
+
+# Show percentages in the pie chart
+plt.pie(sizes, labels=labels_to_plot, autopct='%1.1f%%')
+plt.axis('equal')
+plt.show()
---
+++ src/exploration/count_label_occurrence_per_img.py
@@ -0,0 +1,59 @@
+import os
+import matplotlib.pyplot as plt
+
+# Define the path to the labels folder
+folder_of_this_file = os.path.dirname(__file__)
+labels_folder_path = os.path.join(folder_of_this_file, 'labels')
+
+# Read all filenames in the labels folder
+filenames = os.listdir(labels_folder_path)
+
+# Filter txt files
+filenames = [x for x in filenames if x.endswith('.txt')]
+
+label_occurrences_per_img = {}
+
+
+def get_lines_from_file(filepath):
+    f = open(filepath, 'r')
+    content = f.read()
+    lines = content.splitlines()
+    f.close()
+    return lines
+
+
+def get_labels_occurrence_from_lines(lines):
+    labels = {}
+    for line in lines:
+        try:
+            line_split = line.split(' ')
+            label = line_split[0]
+            if label in labels:
+                labels[label] += 1
+            else:
+                labels[label] = 1
+        except:
+            pass
+    return len(labels.keys())
+
+
+for filename in filenames:
+    filepath = os.path.join(labels_folder_path, filename)
+    lines = get_lines_from_file(filepath)
+    labels_occurrences = get_labels_occurrence_from_lines(lines)
+
+    if labels_occurrences in label_occurrences_per_img:
+        label_occurrences_per_img[labels_occurrences] += 1
+    else:
+        label_occurrences_per_img[labels_occurrences] = 1
+
+labels_to_plot = []
+sizes = []
+for occurrences_count in label_occurrences_per_img:
+    labels_to_plot.append(occurrences_count)
+    sizes.append(label_occurrences_per_img[occurrences_count])
+
+plt.bar(labels_to_plot, sizes, align='center')
+plt.xlabel('Number of labels per image')
+plt.xticks(labels_to_plot)
+plt.show()
